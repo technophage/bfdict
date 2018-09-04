2bd02ef2dd8325a9c5288a12464f1657be052d0d
technophage <g0r@technophage.net>
2018-09-04 13:47:46 +0100

    ..

--- mutaplasmid.py
+++ mutaplasmid.py
@@ -19,11 +19,19 @@ password, they have to change it. Making 99% of ppl add or increment a number to
 the back.
 
 
-Sound simple enough, but the possible mutations for a simple word like 'Balls'
+Sounds simple enough, but the possible mutations for a simple word like 'Jonathan'
 are obscence, and definatly something that should be done by a machine.
 
-This will cause an epic amount of bloat, and as such id stick to simple word lists
-like names, sports teams and the like.
+
+With all the options turned on This will cause an epic amount of bloat ..
+
+.. A single word could easily produce ~50k variations.
+
+With all options on single targeted names, teams or places tailored for the
+potential target are recomended.
+
+Other than that simple lists like names, sports teams and places with more
+conservative option sets will work without too much crunching.
 
 
 
@@ -33,12 +41,13 @@ like names, sports teams and the like.
 
 import sys
 import os
+import re
 from optparse import OptionParser
 
 
 
 
-def mutate(word, cap, num, yrs, sym, wcs):
+def mutate(word, cap, num, yrs, sym, wcs, dbg=False, rs=False):
 
 
     mutations = []
@@ -48,7 +57,7 @@ def mutate(word, cap, num, yrs, sym, wcs):
     nbnds = range(0, 20)
     years = range(1950, 2020)
     
-    wcsl = {'e':'3', 'o':'0', 'i':'1', 't':'7', 's':'5', 'a':'4', 'S':'$', 'E':'£'}
+    wcsl = {'e':'3', 'o':'0', 'i':'1', 't':'7', 's':'5', 'a':'4', 'S':'$'}
     symb = ['@','!','$','.']
 
 
@@ -71,9 +80,9 @@ def mutate(word, cap, num, yrs, sym, wcs):
     # pre-number symbol
     
     if sym:
-        prbwl = list(mutations)
+        bwl = set(list(mutations))
 
-        for bw in prbwl:
+        for bw in bwl:
             for s in symb:
                 mutations.append('{}{}'.format(bw, s))
         
@@ -84,9 +93,9 @@ def mutate(word, cap, num, yrs, sym, wcs):
 
     if num or yrs:
 
-        nbwl = list(mutations)
+        bwl = set(list(mutations))
 
-        for bw in nbwl:
+        for bw in bwl:
 
             if num:
                 for n in nbnds:
@@ -107,9 +116,9 @@ def mutate(word, cap, num, yrs, sym, wcs):
     # post-number symbol
 
     if sym:
-        pobwl = list(mutations)
+        bwl = set(list(mutations))
 
-        for bw in pobwl:
+        for bw in bwl:
             for s in symb:
                 mutations.append('{}{}'.format(bw, s))
 
@@ -118,40 +127,47 @@ def mutate(word, cap, num, yrs, sym, wcs):
     
 
 
-    '''
+    
     # character substitution
 
 
     if wcs:                                                             # char substitutions
-        tw = word
-        for n in range(0, (len[tw] - 1)):
-            try:
-                if wcsl[tw[n]]:
-                    tw[n] = wcsl[tw[n]]
-                    mutations.append(tw)
-                    for w in mutations:
-                        print(w)
+        lc = 1
+        while lc <= len(wcsl):
+            
+            bwl = set(list(mutations))
+            
+            for bw in bwl:
+                for s in wcsl:
+                    mutations.append(re.sub(s, wcsl[s], bw))
+                    
+            lc += 1   
+                
 
-            except:
-                pass
-    '''
+
 
 
     # sort to ensure unique list
 
-    unique_mutations = sorted(set(mutations))
+    unique_mutations = set(mutations)
 
 
     # return mutations
 
-    ##############################
-    ##############################
-    # debug dump
-    
-    for w in unique_mutations:
-            print("\t{}".format(w))
+
+
 
     ##############################
+    # debug
+
+    if dbg:
+        for w in unique_mutations:
+            print("\t{}".format(w))
+            
+    if rs:
+        print len(unique_mutations)
+
+    #
     ##############################
 
 def main():
@@ -161,13 +177,18 @@ def main():
     # process command switches
 
     parser = OptionParser()
-    parser.add_option("-i", action="store", type="string", dest="input_file", help="input file")
-    parser.add_option("-o", action="store", type="string", dest="output_file", help="output file")
-    parser.add_option("-a", action="store_true", dest="all", help="Select all modifications", default=False)
-    parser.add_option("-w", action="store_true", dest="wcs", help="Character substitutions", default=False)
+    parser.add_option("-i", action="store", type="string", dest="input_file", help="Input file")
+    parser.add_option("-o", action="store", type="string", dest="output_file", help="Output file")
+    parser.add_option("-c", action="store_true", dest="cap", help="Case modification", default=False)
     parser.add_option("-n", action="store_true", dest="num", help="Add iterative numbers", default=False)
     parser.add_option("-y", action="store_true", dest="yrs", help="Add years", default=False)
-    parser.add_option("-c", action="store_true", dest="cap", help="Case modification", default=False)
+    parser.add_option("-w", action="store_true", dest="wcs", help="Character substitutions", default=False)
+    parser.add_option("-a", action="store_true", dest="all", help="Select all options", default=False)
+    # advanced tailoring
+    #parser.add_option("--year-from", action="store", type="int", dest="year-from", help="",default=1950)
+    #parser.add_option("--year-to", action="store", type="int", dest="year-to", help="",default=2020)
+    #parser.add_option("--num-from", action="store", type="int", dest="num-from", help="",default=0)
+    #parser.add_option("--num-to", action="store", type="int", dest="num-to", help="",default=20)
 
     (options, args) = parser.parse_args()
 
